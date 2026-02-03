2fa8b41acdc9ce8435b57f6c8751f7c3edb02639
hynever <hynever@163.com>
2026-02-03 16:43:07 +0800

    add user & department repo

---
+++ repository/__init__.py
@@ -0,0 +1,5 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+class BaseRepo:
+    def __init__(self, session: AsyncSession):
+        self.session = session
---
+++ repository/user_repo.py
@@ -0,0 +1,84 @@
+from . import BaseRepo
+from models.user import UserModel, DingdingUserModel, DepartmentModel
+from sqlalchemy import select, delete
+from typing import Sequence
+
+
+class UserRepo(BaseRepo):
+    async def create_user(self, user_data: dict) -> UserModel:
+        user = UserModel(**user_data)
+        self.session.add(user)
+        return user
+
+    async def get_by_id(self, user_id: str) -> UserModel | None:
+        user = await self.session.scalar(
+            select(UserModel).where(UserModel.id==user_id)
+        )
+        return user
+
+    async def get_by_email(self, email: str) -> UserModel | None:
+        user = await self.session.scalar(
+            select(UserModel).where(UserModel.email == email)
+        )
+        return user
+
+    async def get_user_list(
+        self,
+        page: int = 1,
+        size: int = 10,
+        department_id: str|None = None
+    ) -> Sequence[UserModel] | None:
+        stmt = select(UserModel)
+        if department_id:
+            stmt = stmt.where(UserModel.department_id==department_id)
+        limit = size
+        offset = (page-1)*size
+        stmt = stmt.limit(limit).offset(offset).order_by(UserModel.created_at.desc())
+        users = await self.session.scalars(stmt)
+        return users.all()
+
+    async def set_dingding_user(self, user_id: str, dingding_user_data: dict) -> DingdingUserModel:
+        user = await self.get_by_id(user_id)
+        if not user:
+            raise ValueError("设置钉钉的用户不存在！")
+
+        dingding_user = await self.session.scalar(
+            select(DingdingUserModel).where(DingdingUserModel.user_id==user_id)
+        )
+        if dingding_user:
+            for key, value in dingding_user_data.items():
+                setattr(dingding_user, key, value)
+        else:
+            dingding_user = DingdingUserModel(**dingding_user_data, user_id=user_id)
+            self.session.add(dingding_user)
+        return dingding_user
+
+
+class DepartmentRepo(BaseRepo):
+    async def create_department(self, department_data: dict) -> DepartmentModel:
+        department = DepartmentModel(**department_data)
+        self.session.add(department)
+        return department
+
+    async def get_by_id(self, department_id: str) -> DepartmentModel | None:
+        department = await self.session.scalar(
+            select(DepartmentModel).where(DepartmentModel.id==department_id)
+        )
+        return department
+
+    async def get_by_name(self, department_name: str) -> DepartmentModel | None:
+        department = await self.session.scalar(
+            select(DepartmentModel).where(DepartmentModel.name == department_name)
+        )
+        return department
+
+    async def get_department_list(self):
+        departments = await self.session.scalars(
+            select(DepartmentModel)
+        )
+        return departments.all()
+
+    async def delete_department(self, department_id: str) -> None:
+        await self.session.execute(
+            delete(DepartmentModel).where(DepartmentModel.id==department_id)
+        )
