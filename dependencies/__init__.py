7d5b11aba737d0e2996ed8e96fb3fca9d32c9143
hynever <hynever@163.com>
2026-02-04 17:18:06 +0800

    visit limit

--- dependencies/__init__.py
+++ dependencies/__init__.py
@@ -1,6 +1,11 @@
 from models import AsyncSessionFactory, AsyncSession
 from core.auth import AuthHandler
+from fastapi import Depends, HTTPException, status
 
+from models.user import UserModel
+from repository.user_repo import UserRepo
+
+auth_handler = AuthHandler()
 
 async def get_session_instance():
     session: AsyncSession = AsyncSessionFactory()
@@ -10,4 +15,28 @@ async def get_session_instance():
         await session.close()
 
 async def get_auth_handler():
-    return AuthHandler()
+    return auth_handler
+
+def get_user_id(
+    iss: str = Depends(auth_handler.auth_access_dependency)
+) -> str:
+    return iss
+
+async def get_current_user(
+    user_id: str = Depends(get_user_id),
+    session: AsyncSession = Depends(get_session_instance)
+) -> UserModel:
+    async with session.begin():
+        user_repo = UserRepo(session)
+        user: UserModel = await user_repo.get_by_id(user_id)
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="该用户不存在！")
+        return user
+
+async def get_super_user(
+    current_user: UserModel = Depends(get_current_user),
+) -> UserModel:
+    if current_user.is_superuser:
+        return current_user
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="权限不足，无法访问！")
